e9dac100d31717f2f15b6a718aa88a8fa45722ed
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-06-28 16:08:12 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Iuliia Volkova on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
